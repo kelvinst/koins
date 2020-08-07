38992f5ca825d3573d72299e8f761bfa87e97b11
kelvinst <kelvin.stinghen@gmail.com>
2020-08-07 14:24:57 -0300

    Initial Commit

---
+++ koins/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  koins
+//
+//  Created by Kelvin Stinghen on 07/08/20.
+//  Copyright © 2020 0K Software. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
